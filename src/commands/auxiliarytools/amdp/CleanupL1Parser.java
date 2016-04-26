029c135f86d97cd5934fe1889c1720ace422bfff
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2016-04-26 14:25:29 -0400

    Add State Parser for CleanupWorld

---
+++ src/commands/auxiliarytools/amdp/CleanupL1Parser.java
@@ -0,0 +1,90 @@
+package commands.auxiliarytools.amdp;
+
+import burlap.oomdp.core.Domain;
+import burlap.oomdp.core.objects.ObjectInstance;
+import burlap.oomdp.core.states.State;
+import burlap.oomdp.legacy.StateParser;
+import commands.amdp.domain.CleanupL1AMDPDomain;
+import commands.amdp.domain.CleanupWorld;
+import domain.singleagent.sokoban2.Sokoban2Domain;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * CleanupL1Parser.java
+ *
+ * State Parser for Cleanup World L1.
+ *
+ * Created by Sidd Karamcheti on 4/26/16.
+ */
+public class CleanupL1Parser implements StateParser {
+    protected Domain domain;
+
+    public CleanupL1Parser(Domain domain) {
+        this.domain = domain;
+    }
+
+    @Override
+    public String stateToString(State s) {
+        StringBuffer buf = new StringBuffer();
+        List<ObjectInstance> rooms = s.getObjectsOfClass(CleanupWorld.CLASS_ROOM);
+        List<ObjectInstance> doors = s.getObjectsOfClass(CleanupWorld.CLASS_DOOR);
+        List<ObjectInstance> blocks = s.getObjectsOfClass(CleanupWorld.CLASS_BLOCK);
+        List<ObjectInstance> agents = s.getObjectsOfClass(CleanupWorld.CLASS_AGENT);
+
+        for(ObjectInstance o : rooms){
+            buf.append("room,");
+            buf.append(o.getIntValForAttribute(CleanupWorld.ATT_COLOR)).append(",");
+            Set<String> conn = o.getAllRelationalTargets(CleanupL1AMDPDomain.ATT_CONNECTED);
+            for (String str : conn) {
+                buf.append(str).append(",");
+            }
+            buf.deleteCharAt(buf.length() - 1);
+            buf.append(" ");
+        }
+
+        for(ObjectInstance o : doors){
+            buf.append("door,");
+            Set<String> conn = o.getAllRelationalTargets(CleanupL1AMDPDomain.ATT_CONNECTED);
+            for (String str : conn) {
+                buf.append(str).append(",");
+            }
+            buf.deleteCharAt(buf.length() - 1);
+            buf.append(" ");
+        }
+
+        for (ObjectInstance o : blocks) {
+            buf.append("block,");
+            buf.append(o.getIntValForAttribute(CleanupWorld.ATT_COLOR)).append(",");
+            buf.append(o.getIntValForAttribute(CleanupWorld.ATT_SHAPE)).append(",");
+            Set<String> conn = o.getAllRelationalTargets(CleanupL1AMDPDomain.ATT_IN_REGION);
+            for (String str : conn) {
+                buf.append(str).append(",");
+            }
+            buf.deleteCharAt(buf.length() - 1);
+            buf.append(" ");
+        }
+
+        boolean first = true;
+        for (ObjectInstance o : agents) {
+            if (!first) {
+                buf.append(" ");
+            }
+            buf.append("agent,");
+            Set<String> conn = o.getAllRelationalTargets(CleanupL1AMDPDomain.ATT_IN_REGION);
+            for (String str : conn) {
+                buf.append(str).append(",");
+            }
+            buf.deleteCharAt(buf.length() - 1);
+            first = false;
+        }
+
+        return buf.toString();
+    }
+
+    @Override
+    public State stringToState(String s) {
+        return null;
+    }
+}
---
+++ src/commands/auxiliarytools/amdp/EpisodeToTrajectory.java
@@ -0,0 +1,60 @@
+package commands.auxiliarytools.amdp;
+
+import burlap.behavior.singleagent.EpisodeAnalysis;
+import burlap.oomdp.core.Domain;
+import commands.amdp.domain.CleanupL1AMDPDomain;
+import commands.amdp.domain.CleanupWorld;
+import commands.data.Trajectory;
+import commands.data.TrajectoryParser;
+
+import java.io.File;
+
+/**
+ * EpisodeToTrajectory.java
+ *
+ * Reads in a directory of Episode Analysis objects, turns it into a commandsToTask flavored
+ * Trajectory.
+ *
+ * Created by Sidd Karamcheti on 4/26/16.
+ */
+public class EpisodeToTrajectory {
+
+    public static void parseEpisodes(File[] episodes, String outputDir) {
+        CleanupWorld cw = new CleanupWorld();
+        cw.includeDirectionAttribute(true);
+        cw.includePullAction(true);
+        cw.includeWallPF_s(true);
+        cw.includeLockableDoors(true);
+        cw.setLockProbability(0.5);
+
+        Domain domain = cw.generateDomain();
+        CleanupL1AMDPDomain cleanupWorldL1 = new CleanupL1AMDPDomain(domain);
+        cleanupWorldL1.setLockableDoors(false);
+        Domain domainL1 = cleanupWorldL1.generateDomain();
+
+        for (File f: episodes) {
+            String pathName = f.getName();
+            String[] splitPath = pathName.split("|");
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < splitPath.length - 1; i++) {
+                builder.append(splitPath[i]);
+                builder.append(" ");
+            }
+            String naturalCommand = builder.toString();
+            EpisodeAnalysis ep = EpisodeAnalysis.parseFileIntoEA(pathName, domainL1); // TODO
+            Trajectory trajectory = new Trajectory(ep.stateSequence, ep.actionSequence);
+            TrajectoryParser tp = new TrajectoryParser(domainL1, )
+        }
+    }
+
+    public static void main(String[] args) {
+        String episodeDirectory = ""; // TODO
+        String outputDirectory = ""; // TODO
+
+        File dir = new File(episodeDirectory);
+        File[] episodes = dir.listFiles();
+
+        parseEpisodes(episodes, outputDirectory);
+
+    }
+}
