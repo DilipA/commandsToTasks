507ad7d25ece0d247f82aab9dc9fac89b2fb916c
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2016-11-13 18:54:16 -0500

    Fixing for real

--- data/amdpDataNoIRL/gen.py
+++ data/amdpDataNoIRL/gen.py
@@ -7,10 +7,12 @@ if __name__ =="__main__":
     directory = args[1]
 
     for g in os.listdir(directory):
-        with open(os.path.join(directory, g), 'a+') as f:
-            f.write("agentToDoor door0\n")
-            f.write("room,3,door1,door0\n")
-            f.write("room,1,door1 room,0,door0\n")
-            f.write("door,room2,room0\n")
-            f.write("door,room1,room0\n")
-            f.write("block,4,1,room0 agent,room2\n")
+        l = []
+        with open(os.path.join(directory, g), 'r') as f:
+            for line in f:
+                l.append(line.strip())
+
+        with open(os.path.join(directory, g), 'w') as f:
+            f.write(l[0] + "\n")
+            f.write(l[1] + "\n")
+            f.write(" ".join(l[2:]) + "\n")
