c291088756ead7700b82ca1976909451426beb37
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2016-11-13 19:03:54 -0500

    Fixing for real part 2

--- data/amdpDataNoIRL/gen.py
+++ data/amdpDataNoIRL/gen.py
@@ -6,13 +6,26 @@ if __name__ =="__main__":
     args = sys.argv
     directory = args[1]
 
-    for g in os.listdir(directory):
-        l = []
-        with open(os.path.join(directory, g), 'r') as f:
-            for line in f:
-                l.append(line.strip())
+    if directory == "L0":
+        for g in os.listdir(directory):
+            l = []
+            with open(os.path.join(directory, g), 'r') as f:
+                for line in f:
+                    l.append(line.strip())
 
-        with open(os.path.join(directory, g), 'w') as f:
-            f.write(l[0] + "\n")
-            f.write(l[1] + "\n")
-            f.write(" ".join(l[2:]) + "\n")
+            with open(os.path.join(directory, g), 'w') as f:
+                f.write(l[0] + "\n")
+                f.write("west" + "\n")
+                f.write(" ".join(l[2:]) + "\n")
+
+    elif directory == "L1" or directory == "L2":
+        for g in os.listdir(directory):
+            l = []
+            with open(os.path.join(directory, g), 'r') as f:
+                for line in f:
+                    l.append(line.strip())
+
+            with open(os.path.join(directory, g), 'w') as f:
+                f.write(l[0] + "\n")
+                f.write("agentToRoom room1" + "\n")
+                f.write(" ".join(l[2:]) + "\n")
